7430f9ec34128285aa492bbef70bc7e69f37f8e1
aaron <aaron_swan@outlook.com>
2023-01-30 07:06:41 -0800

    Player Spawn Point

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
@@ -7,20 +9,47 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoadIndex = -1;
+        [SerializeField] Transform spawnPoint;
 
         void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
-            {
                 StartCoroutine(Transition());
-            }
         }
 
         IEnumerator Transition()
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoadIndex);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayerPosition(otherPortal);
+
             Destroy(gameObject);
+        }
+
+
+        Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal != this)
+                    return portal;
+            }
+
+            return null;
+        }
+
+
+        
+        void UpdatePlayerPosition(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            
+            // player.transform.position = otherPortal.spawnPoint.position;
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);            
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+
         }
     }
 }
