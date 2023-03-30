a71a10558f1ab887b4a2521de1aadd9fa8dba175
aaron <aaron_swan@outlook.com>
2023-03-30 14:34:08 -0700

    Updating The Damage Text Value

--- Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -8,9 +8,11 @@ namespace RPG.UI.DamageText
     {
         [SerializeField] DamageText damageTextPrefab = null;
 
+
         public void Spawn(float damageAmount)
         {
             DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+            instance.SetValue(damageAmount);
         }
     }
 }
